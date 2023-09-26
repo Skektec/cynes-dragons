8cc912c50438b72cb4a0b306d5d276dc70f89c86
Boys <boys@Boyss-MacBook-Pro.local>
2023-09-26 19:23:44 +0200

    Development work

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-  organization: "YOUR_ORG_ID",
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  organization: process.env.ORG,
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
 const response = await openai.listEngines();
 
 let ac = Number;
@@ -11,23 +11,45 @@ let health = Number;
 let xp = Number;
 let hunger = Number;
 
-const getResponse = async () => {
-  const repsone = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "user",
-        content: `Suggest 5 catchy titles for a blog post about ${userInput}`,
-      },
-    ],
-    temperture: 0,
-    max_tokens: 500,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presense_penalty: 0.0,
-  });
+let start = prompt(
+  "Welcome to Cynes Dragons. Type begin when you would like to begin."
+);
 
-  console.log(response.data.choices[0].message);
-};
+if ((start = "begin")) {
+  let responseContent = ` You are going to be the "Dungeon Master" for a role playing game. You will explain the current
+  and generate incounters of various sort. In this game there is Armor defined as AC which is a number.
+  Health defined as health which is a number. Experience defined as xp which is a number. Hunger
+  defined as hunger which is a number. The value of each of these will be provided with each prompt
+  which will also contain the actions the user wants to take. You will provide short explainations of the
+  surrounding area limited to 4 sentences. Every enemy will have AC, health and xp. Provide everything
+  in a json array and only the json array.`;
 
-getResponse();
+  console.log(responseContent);
+
+  const startGame = async () => {
+    const repsone = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `${responseContent}`,
+        },
+      ],
+      temperture: 0,
+      max_tokens: 500,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presense_penalty: 0.0,
+    });
+
+    console.log(response.data.choices[0].message);
+  };
+
+  startGame();
+}
+
+// content: `Action: ${userInput}
+//                     AC: ${ac}
+//                     Health: ${health}
+//                     Experience: ${xp}
+//                     Hunger: ${hunger}`;
